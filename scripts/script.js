6e514cc3f89caaf53c13355a74816776fc279603
Solomon <gbolahanbalogun5@gmail.com>
2020-04-06 23:07:18 +0100

    Update path to serviceWorker.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
         navigator.serviceWorker
-            .register('/serviceWorker.js')
+            .register('../serviceWorker.js')
             .then(res => console.log('Service worker registered'))
             .catch(err => console.log('Service worker not registered', err))
     }) 
